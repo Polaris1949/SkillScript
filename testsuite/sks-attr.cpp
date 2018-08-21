13193415022023fd7afd3895d368713ce59eee40
Polaris1949 <Polaris1949@126.com>
2018-08-21 22:25:16 +0800

    Update test for attribute

--- testsuite/sks-attr.cpp
+++ testsuite/sks-attr.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
-    sks::attribute a{'a', 'b'};
+    sks::attribute a{'a', 'l'};
     std::cout << sks::attr_to(a) << '\n';
+    std::cout << sks::attr_to(sks::attr_from('b')) << '\n';
 }
