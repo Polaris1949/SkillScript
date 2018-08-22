3185e42e90605e9755099cb96f3a4c9e0e76f333
Polaris1949 <Polaris1949@126.com>
2018-08-22 17:07:25 +0800

    Add version

---
+++ include/version.h
@@ -0,0 +1,15 @@
+#ifndef _SKS_VERSION_H
+#define _SKS_VERSION_H 1
+
+#include <string>
+
+namespace sks
+{
+
+#define SKS_VERSION "1.1.0"
+
+std::string version();
+
+}
+
+#endif
---
+++ src/version.cpp
@@ -0,0 +1,10 @@
+#include "version.h"
+
+namespace sks
+{
+
+std::string
+version()
+{ return SKS_VERSION; }
+
+}
